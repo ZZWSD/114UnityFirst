eebc79395612560bab540208d78f73f124bbcc6a
ZZWSD <125881299+ZZWSD@users.noreply.github.com>
2025-12-18 04:38:20 +0800

    1128

---
+++ Assets/StarterAssets/ThirdPersonController/Scripts/FindNPC.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class FindNPC : MonoBehaviour
+{
+    public float 最長距離 = 5f;
+    float 最短距離 = 10f;
+    GameObject 目標敵人 = null;
+    GameObject[] 所有敵人;
+    public GameObject 玩家;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (玩家 == null) 玩家 = GameObject.FindGameObjectWithTag("Player");
+        所有敵人 = GameObject.FindGameObjectsWithTag("Enemy");
+        if(所有敵人.Length == 0)
+        {
+            目標敵人 = null;
+            return;
+        }
+        目標敵人 = null;
+        最短距離 = 10f;
+        foreach(GameObject 敵人 in 所有敵人)
+        {
+            float 距離 = Vector3.Distance(玩家.transform.position,敵人.transform.position);
+                if (距離 < 最短距離)
+            {
+                最短距離 = 距離;
+                目標敵人 = 敵人;
+            }
+        }
+
+        if(最短距離 > 最長距離)
+        {
+            目標敵人 = null;
+        }
+        Vector3 瞄準位置 = 目標敵人.transform.position;
+        瞄準位置.y = 1.45f;
+        this.transform.position = 目標敵人.transform.position;
+    }
+}
